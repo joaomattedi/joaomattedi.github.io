870fe14fbd24c4a6caa5e9b799b9721627e2e4c8
João Mateus <69877286+joaomattedi@users.noreply.github.com>
2026-06-13 16:04:55 -0300

    feat: add routes for Workout, Routine, Diet

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home';
 import Finance from '../pages/Finance';
+import Workout from '../pages/Workout';
+import Routine from '../pages/Routine';
+import Diet from '../pages/Diet';
 
 export default function AllRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Finance />} />
       <Route path="/finance" element={<Finance />} />
+      <Route path="/workout" element={<Workout />} />
+      <Route path="/routine" element={<Routine />} />
+      <Route path="/diet" element={<Diet />} />
     </Routes>
   );
 }
